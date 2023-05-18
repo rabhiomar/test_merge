faff5dd7b7c3d288e8d4a9f43bf7fd796efa2afe
omar rabhi <amor.rabhi@elite-group.fr>
2023-05-18 15:59:06 +0200

    initial commit

---
+++ src/main/java/com/example/demo/A7DockerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A7DockerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A7DockerProjectApplication.class, args);
+	}
+
+}
